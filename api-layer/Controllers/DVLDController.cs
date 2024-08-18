cc153b9ef2f689e06b7b2f09f0a1053bf8ba9acc
ayaalshouha <ayaalshouha12@gmail.com>
2024-08-18 13:32:16 +0300

    add main controller

---
+++ api-layer/Controllers/DVLDController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api_layer.Controllers
+{
+    public class DVLDController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
