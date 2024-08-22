9ef7e036358eda8a3a023bed9747e3180750338b
ayaalshouha <ayaalshouha12@gmail.com>
2024-08-22 14:38:00 +0300

    add application controller

---
+++ api-layer/Controllers/ApplicationController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api_layer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public class ApplicationController : Controller
+    {
+       
+    }
+}
