ef86ebb347a69dcb8a8df3106c0cf47a0e1103e5
ayaalshouha <ayaalshouha12@gmail.com>
2024-08-22 18:51:11 +0300

    licenses class data layer

--- DataLayer/LicenseClassesData.cs
+++ DataLayer/LicenseClassesData.cs
@@ -6,107 +6,103 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Net.Mime.MediaTypeNames;
+using DTOsLayer;
 
 namespace DataLayer
 {
     public class LicenseClassesData
     {
-        public static bool getClassInfo(int ClassID, ref stLicenseClass Class_) 
+        public static async Task<LicenseClass> getClassInfo(int ClassID) 
         {
-            bool isFound = false;
-            SqlConnection connection = new SqlConnection(DataSettings.ConnectionString);
             try
             {
-                string Query = "select * from LicenseClasses where ID = @ClassID";
-
-                SqlCommand command = new SqlCommand(Query, connection);
-                command.Parameters.AddWithValue("@ClassID", ClassID);
-
-                connection.Open();
-                SqlDataReader reader = command.ExecuteReader();
-                while (reader.Read())
+                using (SqlConnection connection = new SqlConnection(DataSettings.ConnectionString))
                 {
-                    isFound = true;
-                    Class_.ID = (int)reader["ID"];
-                    Class_.ClassName = (string)reader["Class"];
-                    Class_.Description = (string)reader["Description"];
-                    Class_.Fees = (decimal)reader["Fees"];
-                    Class_.ValidityYears = (byte)reader["ValidityYears"];
-                    Class_.MinAgeAllowed = (byte)reader["MinimumAgeAllowed"];
-                }
-                reader.Close();
+                    string Query = "select * from LicenseClasses where ID = @ClassID";
+                    using (SqlCommand command = new SqlCommand(Query, connection))
+                    {
+                        command.Parameters.AddWithValue("@ClassID", ClassID);
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (await reader.ReadAsync())
+                            {
+                                return new LicenseClass
+                                    (
+                                        reader.GetInt32(reader.GetOrdinal("ID")),
+                                        reader.GetString(reader.GetOrdinal("Class")),
+                                        reader.GetString(reader.GetOrdinal("Description")),
+                                        reader.GetDecimal(reader.GetOrdinal("Fees")),
+                                        reader.GetByte(reader.GetOrdinal("MinimumAgeAllowed")),
+                                        reader.GetByte(reader.GetOrdinal("ValidityYears"))
+                                    );
+                            }
+                        }
+                    }
+                }  
             }
             catch (Exception ex)
             {
                 DataSettings.StoreUsingEventLogs(ex.Message.ToString());
                 //Console.WriteLine("Error: " + e.Message);
             }
-            finally
-            {
-                connection.Close();
-            }
-            return isFound;
+            return null;
         }
 
 
-        public static bool Update(stLicenseClass Class_)
+        public static async Task<bool> Update(LicenseClass Class_)
         {
             int RowAffected = 0;
-            SqlConnection Connection = new SqlConnection(DataSettings.ConnectionString);
-
             try
             {
-                string Query = @"Update LicenseClasses
+                using (SqlConnection Connection = new SqlConnection(DataSettings.ConnectionString))
+                {
+
+                    string Query = @"Update LicenseClasses
                                 SET Fees = @Fees
-                WHERE ID = @licenseID;";
-
-                SqlCommand Command = new SqlCommand(Query, Connection);
-
-                Command.Parameters.AddWithValue("@licenseID", Class_.ID);
-                Command.Parameters.AddWithValue("@Fees", Class_.Fees);
-
-                Connection.Open();
-                RowAffected = Command.ExecuteNonQuery();
+                                WHERE ID = @licenseID;";
+                    using (SqlCommand Command = new SqlCommand(Query, Connection))
+                    {
+                        Command.Parameters.AddWithValue("@licenseID", Class_.ID);
+                        Command.Parameters.AddWithValue("@Fees", Class_.Fees);
+                        Connection.Open();
+                        RowAffected = await Command.ExecuteNonQueryAsync();
+                    }
+                }
             }
 
             catch (Exception ex)
             {
                 DataSettings.StoreUsingEventLogs(ex.Message.ToString());
-            }
-            finally
-            {
-                Connection.Close();
             }
 
             return RowAffected > 0;
         }
 
-        public static List<string> getAllClassesName()
+        public static async Task<List<string>> getAllClassesName()
         {
             List<string> list = new List<string>();
-            SqlConnection Connection = new SqlConnection(DataSettings.ConnectionString);
             try
             {
-                string Query = "select Class from LicenseClasses;";
-                SqlCommand command = new SqlCommand(Query, Connection);
-
-                Connection.Open();
-                SqlDataReader reader = command.ExecuteReader();
-
-                while (reader.Read())
+                using (SqlConnection Connection = new SqlConnection(DataSettings.ConnectionString))
                 {
-                    list.Add(reader["Class"].ToString());
+                    string Query = "select Class from LicenseClasses;";
+                    using (SqlCommand command = new SqlCommand(Query, Connection))
+                    {
+                        Connection.Open();
+                        using(SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (await reader.ReadAsync())
+                            {
+                                list.Add(reader["Class"].ToString());
+                            }
+                        }
+                    }
                 }
-
-                reader.Close();
             }
             catch (Exception ex)
             {
                 DataSettings.StoreUsingEventLogs(ex.Message.ToString());
-            }
-            finally
-            {
-                Connection.Close();
             }
             return list;
         }
