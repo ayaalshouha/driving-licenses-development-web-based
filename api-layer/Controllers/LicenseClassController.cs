34f9591c37f70bf6c61ebc5301d0798fd52f9c48
ayaalshouha <ayaalshouha12@gmail.com>
2024-08-27 18:24:00 +0300

    licenses class controller

--- api-layer/Controllers/LicenseClassController.cs
+++ api-layer/Controllers/LicenseClassController.cs
@@ -2,11 +2,14 @@
 
 namespace api_layer.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public class LicenseClassController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+       
     }
 }
