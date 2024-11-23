2e9d995479ca18b490b6f9f1057629f233b2d4ff
ayaalshouha <ayaalshouha12@gmail.com>
2024-11-23 13:25:10 +0300

    remove the angular project

--- dvld-presentation-ng/src/app/app.component.ts
+++ dvld-presentation-ng/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [LoginComponent]
 })
 export class AppComponent {
   title = 'DVLD';
--- dvld-presentation-ng/src/main.ts
+++ dvld-presentation-ng/src/main.ts
@@ -1,7 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { appConfig } from './app/appConfig';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
+bootstrapApplication(AppComponent).catch((err) =>
   console.error(err)
 );
