aab069e451897c9c0075e1e0250ddee6c71f33b4
ayaalshouha <ayaalshouha12@gmail.com>
2024-11-25 14:18:03 +0300

    add routes

--- ng-client-app/src/app/dashboard/dashboard.component.ts
+++ ng-client-app/src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginComponent } from '../login/login.component';
+import { LoginService } from '../services/login.service';
+import { single } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
-
+export class DashboardComponent implements OnInit {
+  constructor(private loginService: LoginService, private router: Router) {}
+  ngOnInit() {
+    this.loginService.getLoginStatue().subscribe((loggedIn) => {
+      if (!loggedIn) {
+        this.router.navigate(['/login']);
+      }
+    });
+  }
 }
