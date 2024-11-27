d21a7624e3f8caa799a3a9f0150d74761e615b03
ayaalshouha <ayaalshouha12@gmail.com>
2024-11-27 17:22:38 +0300

    update css

--- ng-client-app/src/app/dashboard/dashboard.component.ts
+++ ng-client-app/src/app/dashboard/dashboard.component.ts
@@ -4,11 +4,12 @@ import { Router, RouterOutlet } from '@angular/router';
 import { CurrentUserService } from '../services/current-user.service';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { TestComponent } from "./test/test.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [HeaderComponent, SidebarComponent, RouterOutlet],
+  imports: [HeaderComponent, SidebarComponent, RouterOutlet, TestComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
