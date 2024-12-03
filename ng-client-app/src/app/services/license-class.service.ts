af5b636a163c419115179ebb06b208b023249913
ayaalshouha <ayaalshouha12@gmail.com>
2024-12-03 20:48:13 +0300

    add pipe

--- ng-client-app/src/app/services/license-class.service.ts
+++ ng-client-app/src/app/services/license-class.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { LICENSE_CLASS_API_ENDPOINT } from '../environments/endpoints/licenseClass.endpoint';
 import { HttpClient } from '@angular/common/http';
 import { LicenseClass } from '../models/license-class.model';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
@@ -10,6 +10,8 @@ export class LicenseClassService {
   constructor(private http: HttpClient) {}
 
   getAllClasses(): Observable<LicenseClass> {
-    return this.http.get<LicenseClass>(LICENSE_CLASS_API_ENDPOINT.allClasses);
+    return this.http
+      .get<LicenseClass>(LICENSE_CLASS_API_ENDPOINT.allClasses)
+      .pipe(tap((res) => console.log(res)));
   }
 }
