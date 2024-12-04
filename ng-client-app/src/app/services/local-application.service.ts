feff0cdf88d1e3df65d374e84f947b301adba868
ayaalshouha <ayaalshouha12@gmail.com>
2024-12-04 17:29:12 +0300

    add local app service

---
+++ ng-client-app/src/app/services/local-application.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { LocalApplication } from '../models/local-application.model';
+import { LOCAL_APPLICATION_API_ENDPOINT } from '../environments/endpoints/local-application.endpoints';
+import { map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LocalApplicationService {
+  constructor(private http: HttpClient) {}
+
+  create(new_application: LocalApplication): Observable<LocalApplication> {
+    return this.http
+      .post<LocalApplication>(LOCAL_APPLICATION_API_ENDPOINT.create, {
+        new_application,
+      })
+      .pipe(
+        map((res) => {
+          return res;
+        })
+      );
+  }
+}
