3734b7b06d665ad5b3333483e68992d165aeecf6
ayaalshouha <ayaalshouha12@gmail.com>
2024-12-04 15:11:35 +0300

    complete person endpoints

--- ng-client-app/src/app/environments/endpoints/person.endpoints.ts
+++ ng-client-app/src/app/environments/endpoints/person.endpoints.ts
@@ -3,5 +3,8 @@ export const PERSON_API_ENDPOINTS = {
   all: `${environment.apiBaseUrl}/Person/All`,
   read: `${environment.apiBaseUrl}/Person/Read?ID=`,
   create: `${environment.apiBaseUrl}/Person/Create`,
-  
+  update: `${environment.apiBaseUrl}/Person/Update?ID=`,
+  delete: `${environment.apiBaseUrl}/Person/Delete?ID=`,
+  isExist: `${environment.apiBaseUrl}/Person/isExistByID?ID=`,
+  isExistByNationalNo: `${environment.apiBaseUrl}/Person/isExistByNationalNo?NationalNumber=`,
 };
--- ng-client-app/src/app/environments/endpoints/user.endpoints.ts
+++ ng-client-app/src/app/environments/endpoints/user.endpoints.ts
@@ -3,7 +3,8 @@ export const USER_API_ENDPOINTS = {
   all: `${environment.apiBaseUrl}/User/All`,
   read: `${environment.apiBaseUrl}/User/Read?ID=`,
   create: `${environment.apiBaseUrl}/User/Create`,
-  update: `${environment.apiBaseUrl}/User/Update`,
+  update: `${environment.apiBaseUrl}/User/Update?ID=`,
+  delete: `${environment.apiBaseUrl}/User/Delete?ID=`,
   readByUsername: `${environment.apiBaseUrl}/User/ReadByUsername?username=`,
   isExist: `${environment.apiBaseUrl}/User/isExistByID?ID=`,
 };
