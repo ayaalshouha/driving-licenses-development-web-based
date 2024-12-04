095a6923222efc2655970a7593d57ce9ed75e3d4
ayaalshouha <ayaalshouha12@gmail.com>
2024-12-04 16:46:55 +0300

    add person model

---
+++ ng-client-app/src/app/models/person.model.ts
@@ -0,0 +1,18 @@
+export interface Person {
+  firstName: string;
+  secondName: string;
+  thirdName: string;
+  lastName: string;
+  nationalNumber: string;
+  address: string;
+  email: string;
+  phoneNumber: number;
+  birthDate: Date;
+  personalPicture: string;
+  nationality: string;
+  gender: string;
+  createdByUserID: number;
+  creationDate: Date;
+  updatedByUserID: number;
+  updatedDate: Date;
+}
