f80ff048e38abbc79391731893e071cb5c585496
ayaalshouha <ayaalshouha12@gmail.com>
2024-12-05 00:02:12 +0300

    setup notication component

--- ng-client-app/src/app/notification-component/notification-component.component.ts
+++ ng-client-app/src/app/notification-component/notification-component.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-notification-component',
   standalone: true,
   imports: [],
   templateUrl: './notification-component.component.html',
-  styleUrl: './notification-component.component.css'
+  styleUrl: './notification-component.component.css',
 })
 export class NotificationComponentComponent {
+  message = signal<string | null>(null);
 
+  showMessage(msg: string) {
+    this.message.set(msg);
+    setTimeout(() => this.message.set(null), 3000); // Hide after 3 seconds
+  }
 }
