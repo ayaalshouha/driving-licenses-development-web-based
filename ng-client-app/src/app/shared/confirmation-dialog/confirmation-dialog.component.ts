963eabc9dc32c83287668620ba2d54befe488e26
ayaalshouha <ayaalshouha12@gmail.com>
2024-12-07 16:04:21 +0300

    setup the confirmation component

--- ng-client-app/src/app/shared/confirmation-dialog/confirmation-dialog.component.ts
+++ ng-client-app/src/app/shared/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-confirmation-dialog',
   standalone: true,
   imports: [],
   templateUrl: './confirmation-dialog.component.html',
-  styleUrl: './confirmation-dialog.component.css'
+  styleUrl: './confirmation-dialog.component.css',
 })
 export class ConfirmationDialogComponent {
+  @Input() isVisible = false;
+  @Input() title = 'Confirm';
+  @Input() message = 'Are you sure?';
+  @Output() confirmed = new EventEmitter<boolean>();
 
+  confirm() {
+    this.confirmed.emit(true);
+    this.isVisible = false;
+  }
+  cancel() {
+    this.confirmed.emit(false);
+    this.isVisible = false;
+  }
 }
