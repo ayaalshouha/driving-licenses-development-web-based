1307ef54eaa1c711735bf7caa2daeaba8b6e5bd2
ayaalshouha <ayaalshouha12@gmail.com>
2024-12-10 21:09:01 +0300

    setup driver service

--- ng-client-app/src/app/services/driver.service.ts
+++ ng-client-app/src/app/services/driver.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { DRIVER_API_ENDPOINT } from '../environments/endpoints/driver.endpoints';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Driver } from '../models/driver.model';
 @Injectable({
   providedIn: 'root',
 })
 export class DriverService {
   constructor(private http: HttpClient) {}
 
-  getAll() {
-    return this.http.get(DRIVER_API_ENDPOINT.all);
+  getAll(): Observable<Driver[]> {
+    return this.http.get<Driver[]>(DRIVER_API_ENDPOINT.all);
   }
 }
