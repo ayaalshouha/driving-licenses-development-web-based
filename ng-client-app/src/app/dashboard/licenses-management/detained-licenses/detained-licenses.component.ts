70fa29128a38c761ef1d1c898800b910ad694978
ayaalshouha <ayaalshouha12@gmail.com>
2024-12-11 00:42:52 +0300

    update

--- ng-client-app/src/app/dashboard/licenses-management/detained-licenses/detained-licenses.component.ts
+++ ng-client-app/src/app/dashboard/licenses-management/detained-licenses/detained-licenses.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
+import { DetainedLicense } from '../../../models/detained-license.model';
+import { FormControl } from '@angular/forms';
+import { DetainedLicenseService } from '../../../services/detained-license.service';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-detained-licenses',
@@ -7,4 +11,42 @@ import { Component } from '@angular/core';
   templateUrl: './detained-licenses.component.html',
   styleUrl: './detained-licenses.component.css',
 })
-export class DetainedLicensesComponent {}
+export class DetainedLicensesComponent {
+  currentPage = 1;
+  pageSize = 5;
+  list: DetainedLicense[] = [];
+  dataDisplayed: DetainedLicense[] = [];
+  private destroyRef = inject(DestroyRef);
+  filter = new FormControl('', { nonNullable: true });
+
+  constructor(private licenseService: DetainedLicenseService) {}
+
+  ngOnInit(): void {
+    const subscription = this.licenseService
+      .all()
+      .pipe(
+        tap((response) => {
+          this.list = response;
+          this.updateDisplayedData(0);
+        })
+      )
+      .subscribe();
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
+
+  updateDisplayedData(startIndex: number = 0) {
+    const endIndex = startIndex + this.pageSize;
+    this.dataDisplayed = this.list.slice(startIndex, endIndex);
+  }
+  onNext() {
+    if (this.currentPage * this.pageSize < this.list.length) {
+      this.currentPage++;
+      this.updateDisplayedData((this.currentPage - 1) * this.pageSize);
+    }
+  }
+  onPrevious() {
+    this.currentPage--;
+    this.updateDisplayedData((this.currentPage - 1) * this.pageSize);
+  }
+}
