050b5f9e890e4d8e1a47aecf17ff14a164cd09ad
ayaalshouha <ayaalshouha12@gmail.com>
2024-12-15 09:49:20 +0300

    dynamically add test types into dropdown list

--- ng-client-app/src/app/dashboard/appointments/make-appointment/make-appointment.component.ts
+++ ng-client-app/src/app/dashboard/appointments/make-appointment/make-appointment.component.ts
@@ -6,6 +6,9 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { TestType } from '../../../models/test-type.model';
+import { TestTypesService } from '../../../services/test-type.service';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-make-appointment',
@@ -16,6 +19,16 @@ import {
 })
 export class MakeAppointmentComponent {
   testTypes: TestType[] = [];
+  constructor(private testTypeService: TestTypesService) {
+    const subscription = this.testTypeService
+      .all()
+      .pipe(
+        tap((response) => {
+          this.testTypes = response;
+        })
+      )
+      .subscribe();
+  }
   filter = new FormControl('', { nonNullable: true });
   current_date = new Date();
   application_info = new FormGroup({
