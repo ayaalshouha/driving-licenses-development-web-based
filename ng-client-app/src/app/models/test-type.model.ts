dc57e350298e268034a33d4f7b07b57fdf1be597
ayaalshouha <ayaalshouha12@gmail.com>
2024-12-15 09:46:22 +0300

    add test type service and model

---
+++ ng-client-app/src/app/models/test-type.model.ts
@@ -0,0 +1,6 @@
+export interface TestType {
+  id: number;
+  typeTitle: string;
+  fees: number;
+  description: string;
+}
---
+++ ng-client-app/src/app/services/test-type.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+import { TEST_TYPE_API_ENDPOINT } from '../environments/endpoints/test-type.endpoints';
+import { TestType } from '../models/test-type.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TestTypesService {
+  constructor(private http: HttpClient) {}
+
+  all(): Observable<TestType[]> {
+    return this.http.get<TestType[]>(TEST_TYPE_API_ENDPOINT.all);
+  }
+  // getFees(type_id: number) {
+  //   return this.http.get<Observable<number>>(
+  //     `${APPLICATION_TYPE_API_ENDPOINT.fees}${type_id}`
+  //   );
+  // }
+
+  // getApplicationTypeById(id: number): Observable<ApplicationType> {
+  //   return this.http.get<ApplicationType>(`${this.apiUrl}/${id}`);
+  // }
+}
