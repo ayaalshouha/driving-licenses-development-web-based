6929fed587ae8c6dd9eff1b0fc5c2e2c709d5720
ayaalshouha <ayaalshouha12@gmail.com>
2024-12-18 15:01:41 +0300

    add appointments_view endpoint

--- ng-client-app/src/app/environments/endpoints/appointment.endpoints.ts
+++ ng-client-app/src/app/environments/endpoints/appointment.endpoints.ts
@@ -2,6 +2,7 @@ import { environment } from '../environment';
 
 export const APPOINTMENT_API_ENDPOINT = {
   add: `${environment.apiBaseUrl}/appointment`,
+  all: `${environment.apiBaseUrl}/appointment/appointments-view`,
   active_appointments: (test_type: number, local_app: number) =>
     `${environment.apiBaseUrl}/appointment/active-appointments-exist/by-test-type/${test_type}/local-app/${local_app}`,
 };
