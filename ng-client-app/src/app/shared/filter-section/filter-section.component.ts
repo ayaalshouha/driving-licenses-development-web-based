60ed4edf9d404229daf9011774b0cc3fd36e24ee
ayaalshouha <ayaalshouha12@gmail.com>
2024-12-20 14:39:15 +0300

    add filter section component

---
+++ ng-client-app/src/app/shared/filter-section/filter-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './filter-section.component.html',
+  styleUrl: './filter-section.component.css'
+})
+export class FilterSectionComponent {
+
+}
