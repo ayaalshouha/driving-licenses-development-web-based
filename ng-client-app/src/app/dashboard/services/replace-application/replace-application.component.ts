dee2ef12da50a3abf05ef485daaff5a6c49e41b5
ayaalshouha <ayaalshouha12@gmail.com>
2024-12-23 15:56:56 +0300

    add mode in component

--- ng-client-app/src/app/dashboard/services/replace-application/replace-application.component.ts
+++ ng-client-app/src/app/dashboard/services/replace-application/replace-application.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 
+enum enMode {
+  damage = 1,
+  lost,
+}
 @Component({
   selector: 'app-replace-application',
   standalone: true,
   imports: [],
   templateUrl: './replace-application.component.html',
-  styleUrl: './replace-application.component.css'
+  styleUrl: './replace-application.component.css',
 })
 export class ReplaceApplicationComponent {
-
+  replace_mode: enMode = enMode.lost;
 }
