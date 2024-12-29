73247c7e39f1351456afa6ae8eb1974354e53a3b
ayaalshouha <ayaalshouha12@gmail.com>
2024-12-29 15:01:18 +0300

    add take test component

---
+++ ng-client-app/src/app/dashboard/appointments/take-test/take-test.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-take-test',
+  standalone: true,
+  imports: [],
+  templateUrl: './take-test.component.html',
+  styleUrl: './take-test.component.css'
+})
+export class TakeTestComponent {
+
+}
