bb8c044837d8de98a077dfb8d10fab4ca74cf078
ayaalshouha <ayaalshouha12@gmail.com>
2024-12-31 16:13:55 +0300

    update

--- ng-client-app/src/app/dashboard/applications-management/preview-application/preview-application.component.ts
+++ ng-client-app/src/app/dashboard/applications-management/preview-application/preview-application.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { DialogWrapperComponent } from '../../../shared/dialog-wrapper/dialog-wrapper.component';
 
 @Component({
   selector: 'app-preview-application',
   standalone: true,
-  imports: [],
+  imports: [DialogWrapperComponent],
   templateUrl: './preview-application.component.html',
-  styleUrl: './preview-application.component.css'
+  styleUrl: './preview-application.component.css',
 })
-export class PreviewApplicationComponent {
-
-}
+export class PreviewApplicationComponent {}
