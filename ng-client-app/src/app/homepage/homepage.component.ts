2c77280fb55d23bf01d3c5f19d5cd038ba83c13d
ayaalshouha <ayaalshouha12@gmail.com>
2025-01-06 16:20:41 +0300

    adding landing template changing header css

--- ng-client-app/src/app/homepage/homepage.component.ts
+++ ng-client-app/src/app/homepage/homepage.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { LandingComponent } from "./landing/landing.component";
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, LandingComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css',
 })
