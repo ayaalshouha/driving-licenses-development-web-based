a957f1ec19bad3319604268c41b6e28ef0fcf745
ayaalshouha <ayaalshouha12@gmail.com>
2025-01-06 14:31:56 +0300

    add header section

--- ng-client-app/src/app/routes/app.routes.ts
+++ ng-client-app/src/app/routes/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { dashboard_routes } from './dashboard.routes';
+import { HomepageComponent } from '../homepage/homepage.component';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'homepage',
     pathMatch: 'full',
+  },
+  {
+    path: 'homepage',
+    component: HomepageComponent,
   },
   {
     path: 'login',
