cfceabccefd6ed6b38c7ef0895af25a7e06fd8c5
ayaalshouha <ayaalshouha12@gmail.com>
2025-01-06 19:49:11 +0300

    add process steps section

--- ng-client-app/src/app/homepage/homepage.component.ts
+++ ng-client-app/src/app/homepage/homepage.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { LandingComponent } from "./landing/landing.component";
 import { ServicesSectionComponent } from "./services-section/services-section.component";
+import { ProcessSectionComponent } from "./process-section/process-section.component";
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [HeaderComponent, LandingComponent, ServicesSectionComponent],
+  imports: [HeaderComponent, LandingComponent, ServicesSectionComponent, ProcessSectionComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css',
 })
