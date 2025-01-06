4260aa2323f50a3623a08f2206047907bee9e753
ayaalshouha <ayaalshouha12@gmail.com>
2025-01-06 16:55:42 +0300

    add services section

--- ng-client-app/src/app/homepage/homepage.component.ts
+++ ng-client-app/src/app/homepage/homepage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { LandingComponent } from "./landing/landing.component";
+import { ServicesSectionComponent } from "./services-section/services-section.component";
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [HeaderComponent, LandingComponent],
+  imports: [HeaderComponent, LandingComponent, ServicesSectionComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css',
 })
