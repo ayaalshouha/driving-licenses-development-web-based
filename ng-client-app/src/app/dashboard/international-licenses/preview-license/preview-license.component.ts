227a39a1577ffc08ae129afffbfa7e37ad8301f4
ayaalshouha <ayaalshouha12@gmail.com>
2025-01-12 20:39:21 +0300

    add preview license component

---
+++ ng-client-app/src/app/dashboard/international-licenses/preview-license/preview-license.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-preview-license',
+  standalone: true,
+  imports: [],
+  templateUrl: './preview-license.component.html',
+  styleUrl: './preview-license.component.css'
+})
+export class PreviewLicenseComponent {
+
+}
