a0b08152770f28c3cdaaca15e8f916c77db4c5b5
ayaalshouha <ayaalshouha12@gmail.com>
2025-01-30 10:12:14 +0300

    remove unneccessary imports

--- ng-client-app/src/app/dashboard/user-account/user-account.component.ts
+++ ng-client-app/src/app/dashboard/user-account/user-account.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { AddEditApplicationComponent } from "../applications-management/add-edit-application/add-edit-application.component";
-import { NewLocalApplicationComponent } from "../services/new-local-application/new-local-application.component";
+import { NewLocalApplicationComponent } from '../services/new-local-application/new-local-application.component';
 
 @Component({
   selector: 'app-user-account',
   standalone: true,
-  imports: [ NewLocalApplicationComponent],
+  imports: [NewLocalApplicationComponent],
   templateUrl: './user-account.component.html',
-  styleUrl: './user-account.component.css'
+  styleUrl: './user-account.component.css',
 })
-export class UserAccountComponent {
-
-}
+export class UserAccountComponent {}
