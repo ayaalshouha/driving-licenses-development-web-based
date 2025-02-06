d3ae81f16cf16152e14b115107f5fd0ebae8f077
ayaalshouha <ayaalshouha12@gmail.com>
2025-02-06 17:11:52 +0300

    add change password template

--- ng-client-app/src/app/dashboard/user-account/change-password/change-password.component.ts
+++ ng-client-app/src/app/dashboard/user-account/change-password/change-password.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-
+import { DialogWrapperComponent } from '../../../shared/dialog-wrapper/dialog-wrapper.component';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-change-password',
   standalone: true,
-  imports: [],
+  imports: [DialogWrapperComponent],
   templateUrl: './change-password.component.html',
-  styleUrl: './change-password.component.css'
+  styleUrl: './change-password.component.css',
 })
 export class ChangePasswordComponent {
-
+  constructor(private location: Location) {}
+  get invalidPassword() {
+    return true;
+  }
+  onChange() {}
+  onCancel() {
+    this.location.back();
+  }
 }
